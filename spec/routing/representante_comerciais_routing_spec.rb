ddeb1a095eeaba7f4d6c7db3b2a9c89d0e3d6b64
Marco Castro <marcodotcastro@gmail.com>
2019-04-07 11:18:34 -0300

    [c] rspec routings

--- spec/routing/representante_comerciais_routing_spec.rb
+++ spec/routing/representante_comerciais_routing_spec.rb
@@ -1,38 +1,44 @@
 require "rails_helper"
 
 RSpec.describe RepresentanteComerciaisController, type: :routing do
+
+  before(:all) do
+    @representante_comercial = create(:representante_comercial)
+    @cliente = @representante_comercial.cliente
+  end
+
   describe "routing" do
 
     it "routes to #index" do
-      expect(:get => "/representante_comerciais").to route_to("representante_comerciais#index")
+      expect(:get => cliente_representante_comerciais_path(@cliente)).to route_to("representante_comerciais#index", cliente_id: @cliente.id.to_s)
     end
 
     it "routes to #new" do
-      expect(:get => "/representante_comerciais/new").to route_to("representante_comerciais#new")
+      expect(:get => new_cliente_representante_comercial_path(@cliente)).to route_to("representante_comerciais#new", cliente_id: @cliente.id.to_s)
     end
 
     it "routes to #show" do
-      expect(:get => "/representante_comerciais/1").to route_to("representante_comerciais#show", :id => "1")
+      expect(:get => cliente_representante_comercial_path(@cliente, @representante_comercial)).to route_to("representante_comerciais#show", :id => @representante_comercial.id.to_s, cliente_id: @cliente.id.to_s)
     end
 
     it "routes to #edit" do
-      expect(:get => "/representante_comerciais/1/edit").to route_to("representante_comerciais#edit", :id => "1")
+      expect(:get => edit_cliente_representante_comercial_path(@cliente, @representante_comercial)).to route_to("representante_comerciais#edit", :id => @representante_comercial.id.to_s, cliente_id: @cliente.id.to_s)
     end
 
     it "routes to #create" do
-      expect(:post => "/representante_comerciais").to route_to("representante_comerciais#create")
+      expect(:post => cliente_representante_comerciais_path(@cliente)).to route_to("representante_comerciais#create", cliente_id: @cliente.id.to_s)
     end
 
     it "routes to #update via PUT" do
-      expect(:put => "/representante_comerciais/1").to route_to("representante_comerciais#update", :id => "1")
+      expect(:put => cliente_representante_comercial_path(@cliente, @representante_comercial)).to route_to("representante_comerciais#update", :id => @representante_comercial.id.to_s, cliente_id: @cliente.id.to_s)
     end
 
     it "routes to #update via PATCH" do
-      expect(:patch => "/representante_comerciais/1").to route_to("representante_comerciais#update", :id => "1")
+      expect(:patch => cliente_representante_comercial_path(@cliente, @representante_comercial)).to route_to("representante_comerciais#update", :id => @representante_comercial.id.to_s, cliente_id: @cliente.id.to_s)
     end
 
     it "routes to #destroy" do
-      expect(:delete => "/representante_comerciais/1").to route_to("representante_comerciais#destroy", :id => "1")
+      expect(:delete => cliente_representante_comercial_path(@cliente, @representante_comercial)).to route_to("representante_comerciais#destroy", :id => @representante_comercial.id.to_s, cliente_id: @cliente.id.to_s)
     end
 
   end
