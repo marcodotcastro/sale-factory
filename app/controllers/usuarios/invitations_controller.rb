533fb5dcb8515e165f308811b8e6759713537900
Marco Castro <marcodotcastro@gmail.com>
2019-05-03 11:00:27 -0300

    [c] Eu como Cliente quero revincular um Representante

--- app/controllers/usuarios/invitations_controller.rb
+++ app/controllers/usuarios/invitations_controller.rb
@@ -1,12 +1,12 @@
 class Usuarios::InvitationsController < Devise::InvitationsController
 
   def create
-    @usuario = Usuario.find_by(email: usuario_params[:email])
+
 
     respond_to do |format|
-      format.html {redirect_to new_usuario_invitation_path, flash: {error: "Convite já enviado"}} if @usuario
-
-      if Usuario.invite!(usuario_params)
+      if revincular
+        format.html {redirect_to root_path, flash: {success: 'Convite enviado com sucesso.'}}
+      elsif Usuario.invite!(usuario_params)
         format.html {redirect_to root_path, flash: {success: 'Convite enviado com sucesso.'}}
       else
         format.html {redirect_to new_usuario_invitation_path, flash: {error: "Convite não enviado."}}
@@ -18,6 +18,19 @@ class Usuarios::InvitationsController < Devise::InvitationsController
 
   def usuario_params
     params.require(:usuario).permit(:nome, :email, :tipo, :password, :password_confirmation, :invited_by_id, :invited_by_type, :invitation_token, :invitation_accepted_at)
+  end
+
+  def revincular
+    usuario = Usuario.find_by(email: usuario_params[:email])
+    representante = usuario.representante
+    if representante
+      representantes = current_usuario.cliente.representantes << usuario.representante
+      if representantes.find(representante.id)
+        return true
+      end
+    end
+
+    return false
   end
 
 end
