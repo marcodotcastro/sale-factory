9d6bd1c55b4be33990f04b8582543e9ac42778a9
Marco Castro <marcodotcastro@gmail.com>
2019-05-06 16:23:01 -0300

    [a] Principal > Representantes; Consultar por:  Representante, Estado, Cidade e População. Default: Estado do cliente

--- app/controllers/clientes/dashboards_controller.rb
+++ app/controllers/clientes/dashboards_controller.rb
@@ -30,15 +30,49 @@ class Clientes::DashboardsController < ApplicationController
   end
 
   def mapa_lojistas
-    @cidades = Cidade.all
 
-    @lojistas = Lojista.all
+    @q_cidades = Cidade.ransack(params[:q_cidades])
+    @cidades = @q_cidades.result(distinct: true)
+
+    @q_lojistas = Lojista.ransack(params[:q_lojistas])
+    @lojista = @q_lojistas.result(distinct: true)
+
   end
 
   def mapa_representantes
-    @cidades = Cidade.all
 
-    @representantes = Representante.all
+    listas_representantes
+
+    consultar_cidades
+
+    consultar_representantes
+
+  end
+
+  private
+
+  def consultar_representantes
+    if params[:representante].present?
+      q_representantes = Representante.ransack(id_eq: params[:representante][:descricao_eq])
+    else
+      q_representantes = Representante.ransack(cidade_estado_eq: current_usuario.cliente.cidade.estado)
+    end
+    @representantes = q_representantes.result(distinct: true)
+  end
+
+  def consultar_cidades
+    if params[:cidade].present?
+      q_cidades = Cidade.ransack(estado_eq: params[:cidade][:estado_eq], descricao_eq: params[:cidade][:descricao_eq], populacao_gteq: params[:cidade][:populacao_gteq])
+    else
+      q_cidades = Cidade.ransack(estado_eq: current_usuario.cliente.cidade.estado)
+    end
+    @cidades = q_cidades.result(distinct: true)
+  end
+
+  def listas_representantes
+    @representante_list = current_usuario.cliente.representantes.order(descricao: :asc)
+    @estado_list = Cidade.select(:estado).distinct.order(estado: :asc)
+    @cidade_list = Cidade.select(:descricao).distinct.order(descricao: :asc)
   end
 
 end
